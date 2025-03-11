bdb70964c61636d748e62ec75725e9d2bdb08233
Jake Rhodes <jakerhodes8@gmail.com>
2025-03-11 08:06:09 -0600

    update PageRankPHATE

--- embed/__init__.py
+++ embed/__init__.py
@@ -1,5 +1,6 @@
 # Unsupervised
 from .phate import PHATE
+from .phatet import PHATET
 from .dm import DM
 from .mds import MDS
 from .tsne import TSNE
@@ -9,27 +10,35 @@ from .umap import UMAP
 from .lapeig import LAPEIG
 from .kpca import KPCA
 from .pca import PCA
+from .ucebra import UCEBRA
 
 # Supervised
 from .eslle import ESLLE
 from .esisomap import ESISOMAP
 from .sumap import SUMAP
-from .stsne import STSNE
+from .estsne import ESTSNE
 from .nca import NCA
 from .snmf import SNMF
 from .plsda import PLSDA
+from .slapeig import SLAPEIG
 from .spca import SPCA
 from .kspca import KSPCA
+from .cebra import CEBRA
+
 
 # RF-Based
 from .rfphate import RFPHATE
+from .rfdm import RFDM
+from .rfkpca import RFKPCA
 from .rfisomap import RFISOMAP
 from .rfumap import RFUMAP
 from .rfmds import RFMDS
 from .rftsne import RFTSNE
+from .rflapeig import RFLAPEIG
 
 unsupervised_methods = [
     PHATE,
+    PHATET,
     DM,
     MDS,
     TSNE,
@@ -38,12 +47,13 @@ unsupervised_methods = [
     UMAP,
     LAPEIG,
     KPCA,
-    PCA
+    PCA,
+    UCEBRA
 ]
 
 
 supervised_methods = [
-    STSNE,
+    ESTSNE,
     ESLLE,
     ESISOMAP,
     SUMAP,
@@ -51,13 +61,19 @@ supervised_methods = [
     SNMF,
     PLSDA,
     SPCA,
-    KSPCA
+    KSPCA,
+    CEBRA
 ]
 
 rf_methods = [
     RFPHATE,
+    RFDM,
     RFMDS,
     RFTSNE,
     RFISOMAP,
-    RFUMAP
+    RFUMAP,
+    RFKPCA,
+    RFLAPEIG
 ]
+
+regression_methods = unsupervised_methods + rf_methods + [PLSDA, CEBRA]
--- embed/phatet.py
+++ embed/phatet.py
@@ -1,5 +1,5 @@
 import rfphate
 
-
+# TODO: Add PageRankPHATE to init file in rfphate
 def PHATET(data, y = None, verbose=False, n_jobs=-1, **kwargs):
     return rfphate.PageRankPHATE(verbose=verbose, n_jobs = n_jobs, **kwargs).fit_transform(data)
